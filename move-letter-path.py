29f71506c3aee5573d8c39dbbc3b246eb914cff9
gfdgd_xi <3025613752@qq.com>
2024-02-15 22:23:11 +0800

    ar支持m

--- move-letter-path.py
+++ move-letter-path.py
@@ -13,4 +13,4 @@ for p in sys.argv[1:]:
             break
     if debPackage != "":
         os.system(f"mkdir -p '{programPath}/{debPackage[0].lower()}/{debPackage.lower()}'")
-        os.system(f"cp -rv '{p}' '{programPath}/{debPackage[0].lower()}/{debPackage.lower()}'")
+        os.system(f"cp -ruv '{p}' '{programPath}/{debPackage[0].lower()}/{debPackage.lower()}'")
