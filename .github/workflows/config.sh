d2860f3c328b0f90c7971c9f9437b0bacdaac0e1
gfdgd_xi <3025613752@qq.com>
2024-04-08 22:19:54 +0800

    新增根文件自动构建

---
+++ .github/workflows/config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "deb [trusted=true] https://github.com/gfdgd-xi/dde15-for-debian12/raw/main/ ./" > /etc/apt/sources.list.d/gxde-temp.list
+echo "deb https://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware" >> /etc/apt/sources.list.d/debian.list
+echo "deb https://deb.debian.org/debian/ bookworm-backports main contrib non-free non-free-firmware" >> /etc/apt/sources.list.d/debian.list
+echo "deb https://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware" >> /etc/apt/sources.list.d/debian.list
+apt update
+apt install gxde-desktop -y
+apt install firmware-linux network-manager-gnome -y
+rm /etc/apt/sources.list.d/gxde-temp.list
+apt update
+apt clean
